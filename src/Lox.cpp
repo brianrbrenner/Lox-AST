67c58c5a733faea13e7cefbdb8c906daf6d68f0e
Brian Brenner <bbrenne6@kent.edu>
2022-05-28 20:22:43 -0400

    wrote methods and header

---
+++ src/Lox.cpp
@@ -0,0 +1,53 @@
+#include "../include/lox/Lox.hpp"
+#include <iostream>
+#include <fstream>
+
+void Lox::Lox::Report(int line, const std::string &location, const std::string &message) {
+    std::cout << "[line " << "] Error" << location << ": " << message;
+    HadError = true;
+};
+
+void Lox::Lox::Error(int line, const std::string &message) {
+    Report(line, "", message);
+}
+
+void Lox::Lox::run(const std::string &source) {
+    // run scanner
+    // run parser
+    // parse statements
+    // interpret statements
+}
+
+void Lox::Lox::runFile(const char* filename) {
+    std::ifstream file{filename};
+    if (!file.good()) {
+       std::printf("Could not open file %s, ", filename);
+       exit(1);
+    }
+
+    std::string line;
+    std::string source;
+    while (std::getline(file, line)) {
+        source += line + "\n";
+    }
+
+    run(source);
+    if (HadError)
+        exit(1);
+}
+
+void Lox::Lox::runPrompt() {
+    std::printf("Lox Version >> %s \n", LOX_VERSION);
+
+    std::string code;
+    while(true) {
+        std::cout << "> ";
+        if (std::getline(std::cin, code)) {
+            run(code);
+            HadError = false;
+        } else {
+            std::cout << std::endl;
+            break;
+        }
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,49 +2,7 @@
 #include <stdio.h>
 #include <cstring>
 #include <fstream>
-
-#define LOX_VERSION "0.0.1"
-
-void run(const std::string &source) {
-    // run scanner
-    // run parser
-    // parse statements
-    // interpret statements
-}
-
-void runFile(const char* filename) {
-    std::ifstream file{filename};
-    if (!file.good()) {
-       std::printf("Could not open file %s, ", filename);
-       exit(1);
-    }
-
-    std::string line;
-    std::string source;
-    while (std::getline(file, line)) {
-        source += line + "\n";
-    }
-
-    run(source);
-    // make some file for failures and error checking
-}
-
-// allows you to add args after running
-void runPrompt() {
-    std::printf("Lox Version >> %s \n", LOX_VERSION);
-
-    std::string code;
-    while(true) {
-        std::cout << "> ";
-        if (std::getline(std::cin, code)) {
-            run(code);
-            // set errors to false here
-        } else {
-            std::cout << std::endl;
-            break;
-        }
-    }
-}
+#include "../include/lox/Lox.hpp"
 
 int main(int argc, char * argv[]) {
     if (std::strlen(argv[0]) > 1) {
@@ -53,6 +11,6 @@ int main(int argc, char * argv[]) {
         // maybe make a runFile(argv[0]) somewhere else?
         std::ofstream file (argv[0]);
     } else {
-        runPrompt();
+        Lox::Lox::runPrompt();
     }
 }
