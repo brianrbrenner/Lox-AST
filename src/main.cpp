1f19bec880da834e5e7b9b5928047a6392b060fb
Brian Brenner <bbrenne6@kent.edu>
2022-05-28 21:23:37 -0400

    fixed main

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char * argv[]) {
     if (std::strlen(argv[0]) > 1) {
         std::cout << "Usage: jlox [script]";
     } else if (std::strlen(argv[0]) == 1) {
-        // maybe make a runFile(argv[0]) somewhere else?
+        Lox::Lox::runFile(argv[0]);
         std::ofstream file (argv[0]);
     } else {
         Lox::Lox::runPrompt();
