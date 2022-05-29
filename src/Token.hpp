bf16bab47820b374642e0fe97f3efa01ae152d54
Brian Brenner <bbrenne6@kent.edu>
2022-05-28 21:30:09 -0400

    dont even know what i did

--- src/Token.hpp
+++ src/Token.hpp
@@ -9,7 +9,7 @@ namespace Lox
 {
 class Token {
     public:
-        Token(TokenType, std::string, any, int);
+        Token(TokenType, const std::string, any, int);
         std::string toString();
     private:
         enum TokenType type; 
